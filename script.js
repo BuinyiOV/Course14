dcbd49058e4600f91ea4f54e61136e04a23e5db9
Buinyi <buinyiow@gmail.com>
2023-05-11 20:50:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,103 @@
+/*
+
+Задача:
+
+У вас есть список учеников, которые хотят поиграть в игру:
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+Но команд может быть только 3 по 3 человека. Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
+
+Внутри она сначала сортирует имена по алфавиту. Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку. Эти группы должны быть массивами. Как итог, функция возвращает новый массив с тремя командами и строкой как 4й элемент.
+
+Пример:
+
+sortStudentsByGroups(students)  =>
+
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: Takesi'
+]
+Если убрать одно студента из списка, то результат будет:
+
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: -'
+]
+А если добавить одного, то:
+
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: Takesi, Somebody'
+]
+То есть, меняется содержимое строки. Все оставшиеся ученики попадают туда.
+
+*/
+
+
+
+`use srtict`
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+	const sortedList = arr.sort ();
+
+	const gamersList = [];
+	
+	let k = 0
+	for (let i = 0; i < 3; i++){
+		let array = [];
+		for (let j = 0; j < 3; j++){
+			array.push (sortedList[j + k])
+			};
+		k = k + 3;
+		gamersList[i] = array;
+	};
+
+	let str = ``;
+	if (arr.length === 9){
+		str = `Оставшиеся студенты: -`;
+	} else {
+		let restOfStud = [sortedList.slice (9)];
+		str = `Оставшиеся студенты: ${restOfStud[0].join(', ')}`;
+	};
+	gamersList[3] = str;
+	console.log(gamersList);
+	return gamersList;
+};
+
+sortStudentsByGroups (students)
+
+/*
+
+EXEMPLE
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
+
+sortStudentsByGroups(students);
+
+*/
